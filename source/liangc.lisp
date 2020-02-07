5518b35df40c855a88cca6bf125f69d676741f75
RuliaChan <ID+ruliachan@users.noreply.github.com>
2020-02-07 22:37:16 +0900

    Implemented new instructions

--- source/liangc.lisp
+++ source/liangc.lisp
@@ -9,17 +9,21 @@
 
   (iseq (make-array 0 :adjustable T :fill-pointer 0))
 
+  (compiled-lvm-version "")
   
   )
 
 
 
 
-(defun liang-compile-program (code &optional (compile-info (make-lvmiseq-compile-infomation)))
+(defun liang-compile-program
+    (code &optional (compile-info (make-lvmiseq-compile-infomation)))
   (dolist (i code)
     (lvmiseq-encode i compile-info))
-
   compile-info)
+
+
+(defun liang-compile-file ())
 
 
 (defmacro gen-iseq (iseq instruction-name operand)
@@ -27,6 +31,8 @@
 
 (defmacro compile-for-prog (ast)
   `(slot-value (liang-compile-program ,ast) 'iseq))
+
+
 
 
 (defun lvmiseq-encode (ast compile-info)
@@ -41,15 +47,45 @@
 
 
         (:EXP
+
+         (when (and (equal (car (second branchbody))
+                       :CALLDEF)
+                    (equal (car branchbody) '=))
+           
+           ; When calling the operator "=",
+           ; if operator's target is a function then Replace a tree to :def
+
+           (setf (car (second branchbody)) :DEF))
          
          (lvmiseq-encode (second branchbody) compile-info)
          (lvmiseq-encode (third  branchbody) compile-info)
          
-         (gen-iseq iseq :CALLEXP `(,(first branchbody))))
+         (gen-iseq iseq :CALLDEF `(,(first branchbody) 2)))
 
 
+        (:CALLDEF
+         
+         (dolist (i (cdr branchbody))
+           (lvmiseq-encode (car i) compile-info))
+
+
+         (gen-iseq iseq :CALLDEF `(,(if (equal (caar branchbody) :NAME)
+                                   (second (car branchbody))
+                                   (error "function's name is not proper"))
+                               ,(length (cdr branchbody)))))
+        
         (:DEF
-         (gen-iseq iseq :PUSHDEF branchbody))
+         
+         (dolist (i (cdr branchbody))
+           (lvmiseq-encode (car i) compile-info)
+           
+           (let* ((treetype-fname (caar branchbody))
+                  (fname (if (equal treetype-fname :NAME)
+                             (second (car branchbody))
+                             (error "Function's name is not proper"))))
+
+
+             (gen-iseq iseq :PUSHDEF `(,fname ,(length (cdr branchbody)))))))
 
 
         (:PROG
@@ -64,4 +100,5 @@
          (gen-iseq iseq :PUSHNAME branchbody))
 
         
-        (T (print "Unknown Tree"))))))
+        (T (print "Undefined Tree ->")
+           (print branchtype))))))
